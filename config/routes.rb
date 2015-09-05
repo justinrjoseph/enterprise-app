7f29469296d59903b92b5945022f4edc17457af1
Justin Joseph <justin.joseph0@yahoo.com>
2015-09-05 23:13:12 +0000

    Nest purchases under invoices

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  resources :purchases
   resources :employees
+  
   resources :companies do
     collection { post :import }
   end
@@ -14,5 +14,7 @@ Rails.application.routes.draw do
   get '/faqs' => 'welcome#faqs'
   get '/pricing' => 'welcome#pricing'
   
-  resources :invoices
+  resources :invoices do
+    resources :purchases, except: [:index], controller: 'invoices/purchases'
+  end
 end
