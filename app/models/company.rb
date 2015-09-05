d246eaf9a169095910dc43ed6fb3c0b65b8d5257
Justin Joseph <justin.joseph0@yahoo.com>
2015-09-05 17:40:26 +0000

    Add CSV downloader

--- app/models/company.rb
+++ app/models/company.rb
@@ -6,4 +6,13 @@ class Company < ActiveRecord::Base
     end
   end
   
+  def self.to_csv
+    CSV.generate do |csv|
+      csv << column_names
+      all.each do |company|
+        csv << company.attributes.values_at(*column_names)
+      end
+    end
+  end
+  
 end
